c0d00af719463bb4662d31000d42adc07f7a18ca
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-10-06 22:47:24 +0530

    Registration Form Validation: firstName, lastName validation complete

--- JavaScript/Projects/RegistrationFormValidation/index.js
+++ JavaScript/Projects/RegistrationFormValidation/index.js
@@ -1,4 +1,25 @@
 function validate() {
 	let firstName = document.getElementById('first-name').value
-	console.log(firstName)
+	let lastName = document.getElementById('last-name').value
+	let email = document.getElementById('email').value
+	let city = document.getElementById('city').value
+	let state = document.getElementById('state').value
+	let zip = document.getElementById('zip').value
+	let tnC = document.getElementById('t-and-c').checked
+
+	if(firstName.length >= 2) {
+		document.getElementById('first-name-valid').style.display = 'block'
+		document.getElementById('first-name-invalid').style.display = 'none'
+	} else {
+		document.getElementById('first-name-invalid').style.display = 'block'
+		document.getElementById('first-name-valid').style.display = 'none'
+	}
+
+	if(lastName.length >= 2) {
+		document.getElementById('last-name-valid').style.display = 'block'
+		document.getElementById('last-name-invalid').style.display = 'none'
+	} else {
+		document.getElementById('last-name-invalid').style.display = 'block'
+		document.getElementById('last-name-valid').style.display = 'none'
+	}
 }
