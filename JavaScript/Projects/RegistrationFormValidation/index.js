844dcff6993db2e67f94ebd652fa3d83dba1d813
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-10-07 23:59:34 +0530

    JavaScript Object Methods, this keyword, Exercises, Registration Form Validation: Email Validation complete

--- JavaScript/Projects/RegistrationFormValidation/index.js
+++ JavaScript/Projects/RegistrationFormValidation/index.js
@@ -22,4 +22,36 @@ function validate() {
 		document.getElementById('last-name-invalid').style.display = 'block'
 		document.getElementById('last-name-valid').style.display = 'none'
 	}
+
+	/*
+		Email:
+		- Include @ - Done
+		- Include . - Done
+		- Atleast 1 character before @
+		- Atleast 2 characters after last .
+	*/
+
+	/* 
+		includes - Returns true if the string contains that character/substring, returns false if not 
+		indexOf - Returns  index of first occurence of that character/substring, else returns -1
+	*/
+
+	/*
+		m@gmail.co
+		Index of .: 7
+		Length of email: 10
+
+		m@gmail.com
+		Index of .: 7
+		Length of email: 11
+	*/
+
+
+	if(email.includes('@') && email.includes('.') && email.indexOf('@') != 0) {
+		document.getElementById('email-valid').style.display = 'block'
+		document.getElementById('email-invalid').style.display = 'none'
+	} else {
+		document.getElementById('email-invalid').style.display = 'block'
+		document.getElementById('email-valid').style.display = 'none'
+	}
 }
