7906955f15d99b72db4b83829d63703a9b12b6c2
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-10-15 15:22:22 +0530

    JavaScript: Function methods - call, apply, bind; Authentication Project: Basic login logic for one user

--- JavaScript/Projects/Authentication/index.js
+++ JavaScript/Projects/Authentication/index.js
@@ -1,9 +1,38 @@
+let userEmail, userPassword
+
 const signUp = () => {
-	document.getElementById('home').style.display = 'none'
-	document.getElementById('sign-up').style.display = 'block'
+	userEmail = document.getElementById('email').value
+	userPassword = document.getElementById('password').value
+
+	document.getElementById('sign-up-form').reset()
+	console.log('Sign up successful!')
 }
 
-const home = () => {
+const signIn = () => {
+	let enteredEmail = document.getElementById('login-email').value
+	let enteredPassword = document.getElementById('login-password').value
+
+	if(enteredEmail === userEmail && enteredPassword === userPassword) {
+		console.log('Access granted!')
+	} else {
+		console.log('Access denied!')
+	}
+}
+
+const goToHome = () => {
 	document.getElementById('home').style.display = 'block'
+	document.getElementById('sign-up').style.display = 'none'
+	document.getElementById('sign-in').style.display = 'none'
+}
+
+const goToSignUp = () => {
+	document.getElementById('sign-up').style.display = 'block'
+	document.getElementById('home').style.display = 'none'
+	document.getElementById('sign-in').style.display = 'none'
+}
+
+const goToSignIn = () => {
+	document.getElementById('sign-in').style.display = 'block'
+	document.getElementById('home').style.display = 'none'
 	document.getElementById('sign-up').style.display = 'none'
 }
